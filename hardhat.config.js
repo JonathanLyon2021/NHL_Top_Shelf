350ea587a4830f9c4db4511cb3a1f405367d86a7
Jonathan Lyon <82686765+JonathanLyon2021@users.noreply.github.com>
2022-11-20 21:03:28 -0500

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,3 +4,14 @@ require("dotenv").config();
 const fs = require("fs");
 const projectId = "460a2af81be44b31aed0e928f26cbc53"
 const privateKey = process.env.PRIVATE_KEY;
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+networks: {
+  hardhat: {
+    chainId: 1337 // 1337 is for Geth //
+  },
+  mumbai: {
+    url: `https://polygon-mumbai.infura.io/v3/${projectId}`,
+    accounts: [privateKey] //priv.key inside git.ignore
+  },
