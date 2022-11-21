0c49b7ab0cbfdba17721f826cddfc4898304c4de
Jonathan Lyon <82686765+JonathanLyon2021@users.noreply.github.com>
2022-11-20 21:03:07 -0500

    Create hardhat.config.js

---
+++ hardhat.config.js
@@ -0,0 +1,6 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-waffle");
+require("dotenv").config();
+const fs = require("fs");
+const projectId = "460a2af81be44b31aed0e928f26cbc53"
+const privateKey = process.env.PRIVATE_KEY;
