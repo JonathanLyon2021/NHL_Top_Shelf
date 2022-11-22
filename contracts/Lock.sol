f84a8dfd2f0b687d80e8dd839a0b1716fe066464
Jonathan Lyon <82686765+JonathanLyon2021@users.noreply.github.com>
2022-11-21 23:46:08 -0500

    Added constructor function

--- contracts/Lock.sol
+++ contracts/Lock.sol
@@ -10,4 +10,13 @@ contract Lock {
 // Todo: Add a constructor that takes a uint for the unlock time and an address for the owner
     event Withdrawal(uint amount, uint when);
     
+      constructor(uint _unlockTime) payable {
+        require(
+            block.timestamp < _unlockTime,
+            "Unlock time should be in the future"
+        );
+
+        unlockTime = _unlockTime;
+        owner = payable(msg.sender);
+    }
 }
