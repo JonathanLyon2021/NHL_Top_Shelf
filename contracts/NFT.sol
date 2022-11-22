49ac53114f4c3f882047c15ff63327b47c1b6545
Jonathan Lyon <82686765+JonathanLyon2021@users.noreply.github.com>
2022-11-21 23:43:04 -0500

    Create NFT.sol

---
+++ contracts/NFT.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzepplin/contracts/token/ERC721/ERC721.sol";
+import "@openzepplin/contracts/token/ERC721/extensions/ERC721Storage.sol";
+import "@openzepplin/contracts/utils/Counters.sol";
+
+contract NFT is ERC721URIStorage {
+   
+    using Counters for Counters.Counter; 
+    Counters.Counter private _tokenIds; 
+    address contractAddress;
+
+}
