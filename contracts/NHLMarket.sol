1f13dfcfbc91b8500312aaba1d9ba16bb8eee6d4
Jonathan Lyon <82686765+JonathanLyon2021@users.noreply.github.com>
2022-11-23 23:59:27 -0500

    Update NHLMarket.sol

--- contracts/NHLMarket.sol
+++ contracts/NHLMarket.sol
@@ -1,1 +1,12 @@
 //TODO: NHLMarket implementation
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzepplin/contracts/utils/Counters.sol";
+import "@openzepplin/contracts/token/ERC721/ERC721.sol";
+import "@openzepplin/contracts/security/ReentracncyGuard.sol";
+//*Security mechanism that gives us a utility called non-Reentrant which will allow us to protect certian txns 
+//*that are interacting with certian contracts ro protect someone hitting this with multipple attempts at the txn, 
+//*and doing shady things to prevent reentrancy attacks
+
+//Todo: Declare the contract
