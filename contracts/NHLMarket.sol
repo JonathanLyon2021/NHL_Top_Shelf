a53eca6831d26eec0ac606fa670e795bc8a2641c
Jonathan Lyon <82686765+JonathanLyon2021@users.noreply.github.com>
2022-11-25 23:59:49 -0500

    Added method createMarketSale()

--- contracts/NHLMarket.sol
+++ contracts/NHLMarket.sol
@@ -63,4 +63,21 @@ contract NHLMarket is ReentracncyGuard {
     //keeping up with the items that have beeen created
 
    //IERC721(nftContract).transferFrom(msg.sender, address(this), tokenId);
+   
+    function createMarketSale(
+        address nftContract,
+        uint256 itemId
+    ) public payable nonReentrant {
+        uint price = idToMarketItem[itemId].price;
+        uint tokenId = idToMarketItem[itemId].tokenId;
+
+        require(msg.value == price, "Please submit the asking price in order to complete the purchase");
+
+        idToMarketItem[itemId].seller.transfer(msg.value);
+        IERC721(nftContract).transferFrom(address(this), msg.sender, tokenId);
+        idToMarketItem[itemId].owner = payable(msg.sender);
+        idToMarketItem[itemId].sold = true;
+        _itemsSold.increment();
+        payable(owner).transfer(listingPrice);
+    } 
 }
