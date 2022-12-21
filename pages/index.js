999fa2ef447df960bfe9c05c7c97d13ce0d5e7f7
Jonathan Lyon <82686765+JonathanLyon2021@users.noreply.github.com>
2022-12-20 21:06:44 -0500

    Home page(index.js) UI, being rendered

--- pages/index.js
+++ pages/index.js
@@ -87,4 +87,42 @@ export default function Home() {
 		return (
 			<h1 className="px-20 py-10 text-3xl">No items in marketplace</h1>
 		);
+	
+	return (
+		<div>
+			<Navbar />
+			<div className="flex justify-center">
+				<div className="px-4" style={{ maxWidth: "1600px" }}>
+					<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4">
+						{nfts.map((nft, i) => (
+							<div
+								key={i}
+								className="border shadow rounded-xl overflow-hidden"
+							>
+								<img src={nft.image} />
+								<div className="p-4">
+									<p
+										style={{ height: "64px" }}
+										className="text-2xl font-semibold"
+									>
+										{nft.name}
+									</p>
+									<div
+										style={{
+											height: "70px",
+											overflow: "hidden",
+										}}
+									>
+										<p className="text-gray-400">
+											{nft.description}
+										</p>
+									</div>
+								</div>
+							</div>
+						))}
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
