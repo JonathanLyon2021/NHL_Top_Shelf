4849b5a436c4bbc15921df6fb888da78af1d23a0
Jonathan Lyon <82686765+JonathanLyon2021@users.noreply.github.com>
2022-12-20 20:57:29 -0500

    Added async function loadNFTs()

--- pages/index.js
+++ pages/index.js
@@ -17,4 +17,17 @@ export default function Home() {
 		loadNFTs();
 	}, []);
   
+	async function loadNFTs() {
+		const provider = new ethers.providers.JsonRpcProvider();
+		const tokenContract = new ethers.Contract(
+			nftaddress,
+			NFT.abi,
+			provider
+		);
+		const marketContract = new ethers.Contract(
+			nftmarketaddress,
+			Market.abi,
+			provider
+		);
+		const data = await marketContract.fetchMarketItems();
 }
