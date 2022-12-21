37c73c13db764943ee1f0f830234fa233a6f8414
Jonathan Lyon <82686765+JonathanLyon2021@users.noreply.github.com>
2022-12-20 21:13:24 -0500

    Updated NavBar component with buttons & links

--- components/navbar.js
+++ components/navbar.js
@@ -2,12 +2,27 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import Link from "next/link";
-import Navbar from "../components/navbar";
 
-export default function Home() {
+export default function Navbar() {
 	return (
 		<div>
-      <Navbar />
+			<nav className="border-b p-6">
+				<p className="text-4xl font-bold">NHL Marketplace</p>
+				     <div className="flex mt-4">
+					<Link href="/">
+						<button className="mr-4 text-pink-500">Home</button>
+					</Link>
+					<Link href="/create">
+						<button className="mr-6 text-pink-500">Sell Top-Shelf NFT</button>
+					</Link>
+					<Link href="/library">
+						<button className="mr-6 text-pink-500">My Top-Shelf NFTs</button>
+					</Link>
+					<Link href="/dashboard">
+						<button className="mr-6 text-pink-500">Dashboard</button>
+					</Link>
+				</div>
+			</nav>
 		</div>
 	);
 }
