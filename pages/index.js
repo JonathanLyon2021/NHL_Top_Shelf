87d3dff5df11600d11b2d3cff9c318ae7afb8dbe
Jonathan Lyon <82686765+JonathanLyon2021@users.noreply.github.com>
2022-12-20 20:58:48 -0500

    Finished updating loadNFTs() method

--- pages/index.js
+++ pages/index.js
@@ -30,4 +30,31 @@ export default function Home() {
 			provider
 		);
 		const data = await marketContract.fetchMarketItems();
+		
+		//this is a json representation from ipfs for instanceof(description, image, name, etc.)
+		const items = await Promise.all(
+			data.map(async (i) => {
+				const tokenUri = await tokenContract.tokenURI(i.tokenId);
+				const meta = await axios.get(tokenUri);
+				let price = ethers.utils.formatUnits(
+					i.price.toString(),
+					"ether"
+				);
+				//we are setting this stuff to the item
+				let item = {
+					price,
+					tokenId: i.tokenId.toNumber(),
+					seller: i.seller,
+					owner: i.owner,
+					image: meta.data.image,
+					name: meta.data.name,
+					description: meta.data.description,
+				};
+				return item;
+			})
+		);
+		setNfts(items);
+		setLoadingState("loaded"); //set loading state to loaded
+		
+	}
 }
