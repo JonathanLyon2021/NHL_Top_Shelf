a463410e17fadf3a6a83f90ae40de2998e0274d9
Jonathan Lyon <82686765+JonathanLyon2021@users.noreply.github.com>
2022-12-22 22:47:53 -0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,38 +1,16 @@
-/* pages/_app.js */
-import '../styles/globals.css'
+import "../styles/globals.css";
 import Link from 'next/link'
 
+
 function MyApp({ Component, pageProps }) {
-  return (
-    <div>
-      <nav className="border-b p-6">
-        <p className="text-4xl font-bold">NHL Marketplace</p>
-        <div className="flex mt-4">
-          <Link href="/">
-            <a className="mr-4 text-pink-500">
-              Home
-            </a>
-          </Link>
-          <Link href="/create-nft">
-            <a className="mr-6 text-pink-500">
-              Sell Top-Shelf NFT
-            </a>
-          </Link>
-          <Link href="/my-nfts">
-            <a className="mr-6 text-pink-500">
-              My Top-ShelfNFTs
-            </a>
-          </Link>
-          <Link href="/dashboard">
-            <a className="mr-6 text-pink-500">
-              Dashboard
-            </a>
-          </Link>
-        </div>
-      </nav>
-      <Component {...pageProps} />
-    </div>
-  )
+	return (
+	<div>
+		<Component {...pageProps} />;
+	</div>
+	)
 }
 
-export default MyApp
+export default MyApp;
+
+
+
