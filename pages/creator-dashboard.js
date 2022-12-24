b4890ad46a895ceb2b26660e77845ea6e539cb7d
Jonathan Lyon <82686765+JonathanLyon2021@users.noreply.github.com>
2022-12-23 23:54:05 -0500

    Updated imports & filename

--- pages/creator-dashboard.js
+++ pages/creator-dashboard.js
@@ -1,6 +1,8 @@
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import {useState} from 'react';
+import { ethers } from "ethers";
+import Web3Modal from "web3modal";
+import {useRouter} from 'next/router';
+import {create as ipfsHttpClient} from 'ipfs-http-client';
 import Link from "next/link";
 import Navbar from "../components/navbar";
 
