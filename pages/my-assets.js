ea02a68659e50b36af5f32f6e37e26e7ff6c5ad2
Jonathan Lyon <82686765+JonathanLyon2021@users.noreply.github.com>
2022-12-23 23:55:24 -0500

    Updated imports &  file name

---
+++ pages/my-assets.js
@@ -0,0 +1,16 @@
+import {useState} from 'react';
+import { ethers } from "ethers";
+import Web3Modal from "web3modal";
+import {useRouter} from 'next/router';
+import {create as ipfsHttpClient} from 'ipfs-http-client';
+import Link from "next/link";
+import Navbar from "../components/navbar";
+export default function Library() {
+    
+	return (
+		<div>
+			<Navbar />
+			<p className="text-4xl font-bold">My Assets</p>
+		</div>
+	);
+}
