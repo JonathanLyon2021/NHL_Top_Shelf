a32af1ea0bf2382b39e27812fa1fcebcc663372d
Jonathan Lyon <82686765+JonathanLyon2021@users.noreply.github.com>
2022-12-25 23:58:36 -0500

    Deleted Navbar Component for now

--- pages/creator-dashboard.js
+++ pages/creator-dashboard.js
@@ -4,12 +4,10 @@ import Web3Modal from "web3modal";
 import {useRouter} from 'next/router';
 import {create as ipfsHttpClient} from 'ipfs-http-client';
 import Link from "next/link";
-import Navbar from "../components/navbar";
 
 export default function Dashboard() {
 	return (
 		<div>
-        <Navbar />
 				<p className="text-4xl font-bold">Dashboard</p>
 		</div>
 	);
